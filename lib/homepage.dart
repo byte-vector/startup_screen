2d2ea90b53d12fec492a453d162920ad29157bfb
byte-vector <asthachaubey87@gmail.com>
2020-10-14 15:27:04 +0530

    Made Some Changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,12 +9,49 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Alpha",
-              style:
-                  TextStyle(fontStyle: FontStyle.italic, color: Colors.black),
-            )
+            Center(
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.cyan,
+                  child: Center(
+                    child: Text(
+                      "Alpha",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic, color: Colors.black),
+                    ),
+                  )),
+            ),
+            Container(
+                color: Colors.pink,
+                width: 100,
+                height: 100,
+                margin: EdgeInsets.all(20),
+                // padding: EdgeInsets.all(45),
+                child: Center(
+                  child: Text(
+                    "Beta",
+                    style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 16.0),
+                  ),
+                )),
+            Container(
+                color: Colors.teal,
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: Text(
+                    "Gama",
+                    style: TextStyle(
+                        fontStyle: FontStyle.italic, color: Colors.black),
+                  ),
+                )),
           ],
         ),
       ),
